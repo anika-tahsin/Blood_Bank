8892b19d06c5ebb7d4ff8a3ed125e2e93def843f
Anika-Tahsin <anikatahsintonu@gmail.com>
2025-09-06 11:52:18 +0600

    package vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+}) //works locally but not on render
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: './postcss.config.js', // This handles Tailwind
+//   },
+// })
